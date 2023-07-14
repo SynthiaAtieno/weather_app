0640eeee8420b166cfaaf012c2ade281f5019aba
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-07-14 14:59:17 +0300

    styling the weather screen

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -15,12 +15,41 @@ class WeatherAppScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.refresh,
-                color: Colors.white,
-              ))
+            onPressed: () {},
+            icon: const Icon(
+              Icons.refresh,
+              color: Colors.white,
+            ),
+          )
         ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "300.6°F",
+                        style: TextStyle(fontSize: 32),
+                      ),
+                      Icon(
+                        Icons.cloud,
+                        size: 80,
+                      ),
+                      Text(
+                        "Rain",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ]),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
